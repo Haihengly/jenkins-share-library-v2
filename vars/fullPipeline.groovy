056481b9cf1ead27160337adcc7ba6c0419ba63e
Haihengly <your-email@example.com>
2026-04-24 15:42:12 +0700

    Initial

--- vars/fullPipeline.groovy
+++ vars/fullPipeline.groovy
@@ -1,47 +1,35 @@
 def call(Map config) {
 
-    pipeline {
-        agent {
-            docker {
-                image 'haihengly/docker-agent:1.2'
-                label 'agent-01'
-                args '''
-                    -v /tmp/npm-cache:/home/jenkins/.npm
-                    -v /var/run/docker.sock:/var/run/docker.sock
-                    -v /ansible:/ansible
-                    --memory=4g
-                    --cpus=2
-                    --group-add 988
-                '''
-            }
-        }
+    node('agent-01') {
 
-        stages {
-            stage('Pipeline') {
-                steps {
-                    script {
+        docker.image('haihengly/docker-agent:1.2')
+            .inside("""
+                -v /tmp/npm-cache:/home/jenkins/.npm
+                -v /var/run/docker.sock:/var/run/docker.sock
+                -v /ansible:/ansible
+                --memory=4g
+                --cpus=2
+                --group-add 988
+            """) {
 
-                        config.stages.each { s ->
+            stage('Pipeline Execution') {
 
-                            stage(s.name) {
-                                stageExecutor(s.type, config)
-                            }
-                        }
+                config.stages.each { s ->
+
+                    stage(s.name) {
+
+                        echo "🚀 Running stage: ${s.name} (${s.type})"
+
+                        stageExecutor(s.type, config)
                     }
                 }
             }
         }
+    }
 
-        post {
-            success {
-                echo "Pipeline succeeded!"
-            }
-            failure {
-                echo "Pipeline failed!"
-            }
-            unstable {
-                echo "Pipeline is unstable!"
-            }
+    post {
+        always {
+            echo "📦 Pipeline finished"
         }
     }
 }
